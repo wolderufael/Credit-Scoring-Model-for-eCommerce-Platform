aaa50365389d3e2fce16b4e71c3e9b5c65db6d54
wolderufael <wolderuphaelkassahun@gmail.com>
2024-10-08 19:13:44 +0300

    train data pipepline

---
+++ cat_breaks_20241008_173147.py
@@ -0,0 +1,6 @@
+breaks_list={
+'ChannelId': ['ChannelId_2','ChannelId_3%,%ChannelId_5%,%ChannelId_1'], 
+'PricingStrategy': ['strat2%,%strat0','strat4%,%strat1'], 
+'ProductCategory': ['tv%,%utility_bill%,%ticket%,%airtime','financial_services%,%data_bundles%,%transport%,%movies%,%other'], 
+'ProductId': ['ProductId_22%,%ProductId_23%,%ProductId_7%,%ProductId_14%,%ProductId_8%,%ProductId_19%,%ProductId_13%,%ProductId_21%,%ProductId_3%,%ProductId_1','ProductId_6%,%ProductId_20%,%ProductId_10%,%ProductId_2','ProductId_4%,%ProductId_11%,%ProductId_27%,%ProductId_15%,%ProductId_5%,%ProductId_24%,%ProductId_16%,%ProductId_9%,%ProductId_12'], 
+'ProviderId': ['ProviderId_2%,%ProviderId_6','ProviderId_4','ProviderId_5','ProviderId_3%,%ProviderId_1']}
--- script/default_estimator_and_WoE_binning.py
+++ script/default_estimator_and_WoE_binning.py
@@ -2,6 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import pandas as pd
+import sidetable
+# %matplotlib inline
+import seaborn as sns
+import scorecardpy as sc
+from monotonic_binning.monotonic_woe_binning import Binning
 
 
 class Estimator:
@@ -56,14 +63,83 @@ class Estimator:
                             rfms['Monetary'].rank(ascending=False) + 
                             rfms['Stability'].rank(ascending=False))
         
+        # Calculate the first quartile (Q1)
+        first_quartile = rfms['RiskScore'].quantile(0.25)
+        # Assign RiskLabel based on whether RiskScore is above or below Q1
+        # rfms['RiskLabel'] = np.where(rfms['RiskScore'] > first_quartile, 1, 0)
+
         # Classify into Good (low risk) and Bad (high risk)
         rfms['RiskLabel'] = np.where(rfms['RiskScore'] > rfms['RiskScore'].median(), 1, 0)
 
         return rfms
 
     def merge_dataframes(self,df1, df2):
+        columns_to_drop = ['TransactionId','TransactionStartTime', 'BatchId', 'AccountId', 'SubscriptionId', 'CurrencyCode', 'CountryCode', 'Value']
+        df1 = df1.drop(columns=columns_to_drop)
+        
         # Merge the two DataFrames on 'CustomerId'
         merged_df = pd.merge(df1, df2, on='CustomerId', how='inner')  
         merged_df.drop(columns='CustomerId',inplace=True)
+        # Define the mapping
+        pricing_strategy_map = {0: 'strat0', 1: 'strat1', 2: 'strat2', 4: 'strat4'}
+
+        # # Replace the values in the PricingStrategy column
+        merged_df['PricingStrategy'] = merged_df['PricingStrategy'].replace(pricing_strategy_map)
         
         return merged_df
+    
+    def split_data(self,df):
+        train, test = sc.split_df(df, 'RiskLabel', ratio = 0.7, seed = 999).values()
+        
+        return train,test
+    
+    def woe_num(self,train,y):  
+        # WoE (Weight of Evidence) Transformation
+        x = train.drop(['RiskLabel', 'ProviderId', 'ProductId', 'ProductCategory', 'ChannelId', 'PricingStrategy'], axis = 1).columns
+        y_var = train['RiskLabel']   
+                        
+        bin_object = Binning(y, n_threshold = 50, y_threshold = 10, p_threshold = 0.35, sign=False)
+        global breaks
+        breaks = {}
+        for i in x:
+            bin_object.fit(train[[y, i]])
+            breaks[i] = (bin_object.bins[1:-1].tolist())
+        return y_var,breaks
+    
+    def bin_catagorical_features(self,train):
+        categorical_columns = ['ProviderId', 'ProductId', 'ProductCategory', 'ChannelId', 'PricingStrategy']
+        bins = sc.woebin(train, y = 'RiskLabel', x = categorical_columns, save_breaks_list = 'cat_breaks')
+        
+    def plot_woe_iv(self,df,breaks):
+        bins_adj = sc.woebin(df, 'RiskLabel', breaks_list= breaks, positive = 'bad|0')
+        sc.woebin_plot(bins_adj)
+        
+        return bins_adj
+        
+    def converting_into_woe_values(self,train,test,bins_adj):
+        train_woe = sc.woebin_ply(train, bins_adj)
+        test_woe = sc.woebin_ply(test, bins_adj)
+        
+        # Merge by index
+        train_final = train.merge(train_woe, how = 'left', left_index=True, right_index=True)
+        test_final = test.merge(test_woe, how = 'left', left_index=True, right_index=True)
+        
+        train_final = train_final.drop(columns = 'RiskLabel_y').rename(columns={'RiskLabel_x':'vd'})
+        test_final = test_final.drop(columns = 'RiskLabel_y').rename(columns={'RiskLabel_x':'vd'})
+        
+        # # Drop transformed variables
+        categorical_columns = ['ProviderId', 'ProductId', 'ProductCategory', 'ChannelId', 'PricingStrategy']
+        train_final = train_final.drop(columns = categorical_columns)
+        test_final = test_final.drop(columns = categorical_columns)
+        
+        return train_final,test_final
+    
+    def calculate_information_value(self,train_final):
+        iv=sc.iv(train_final, y = 'vd')
+        
+        return iv
+        
+    def filter_variables(self,train_final):
+        filtered=sc.var_filter(train_final, y = 'vd')
+        
+        return filtered
